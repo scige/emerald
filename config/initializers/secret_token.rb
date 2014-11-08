396fa2aa8cbfb122ab56286ea0ea62de5a73c99e
scige <panf06@gmail.com>
2014-11-08 09:35:17 +0800

    create project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Emerald::Application.config.secret_token = '5f9f26c247441eb2de2f5fa3d60b946355bb78cec24a59f8201b7f6093f57569f75d27e5df3350136bfadf642353f9e8decd1633de9853cc33a6977a6fefcf1a'
