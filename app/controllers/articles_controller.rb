e6124fe78d94ab95415b6ce8e66046ed70ca74b1
scige <panf06@gmail.com>
2014-11-13 22:08:49 +0800

    finish processing procedure

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,3 +1,5 @@
+# coding: utf-8
+
 class ArticlesController < ApplicationController
   def index
     @articles = Article.all
@@ -5,6 +7,7 @@ class ArticlesController < ApplicationController
 
   def show
     @article = Article.find(params[:id])
+    @result = session[:result]
   end
 
   def new
@@ -19,6 +22,9 @@ class ArticlesController < ApplicationController
     @article = Article.new(params[:article])
 
     if @article.save
+      @words = Word.all.map{|w| w.right}
+      session[:result] = spelling_check(@article.content, @words)
+
       redirect_to @article, notice: 'Article was successfully created.'
     else
       render action: "new"
@@ -40,5 +46,11 @@ class ArticlesController < ApplicationController
     @article.destroy
 
     redirect_to articles_url
+  end
+
+  private
+
+  def spelling_check(article, words)
+    [{:pos=>5, :origin=>"赵净波", :maybe=>"赵静波"}]
   end
 end
--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,20 @@
 module ArticlesHelper
+  def format_highlight(content, result)
+    output = ""
+    last_pos = 0
+    result.each do |r|
+      output += content[0...r[:pos]] + "<span class='highlight-word'>#{r[:origin]}</span>"
+      last_pos += r[:pos] + r[:origin].size
+    end
+    output += content[last_pos...content.size]
+    return output
+  end
+
+  def format_advise(result)
+    output = ""
+    result.each do |r|
+      output += r[:origin] + " -> " + r[:maybe] + "<br>"
+    end
+    return output
+  end
 end
